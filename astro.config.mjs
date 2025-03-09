87c399b52f67e6d1684e41bde99ab05d93c8a8b6
houssam-zouhri <zouhri1230@gmail.com>
2025-03-09 23:25:10 +0000

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://houssam-zouhri.github.io/project',
+  build: {
+    // Specify base URL for the assets
+    base: '/project/',
+  },
+});
